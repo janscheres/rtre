49fab95d5178cd924b0637d32f50d3d60a2e545b
Jan Scheres <jscheresm@gmail.com>
2026-02-02 00:09:37 +0000

    Several improvements

--- server/main.go
+++ server/main.go
@@ -3,9 +3,46 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 )
+
+type Order struct {
+	Price float64
+	Quantity float64
+}
+
+func (o *Order) UnmarshalJSON(data []byte) error {
+	var raw [2]string
+	err := json.Unmarshal(data, &raw)
+	if err != nil {
+		log.Println("ERROR: [JSON] Error parsing order", err)
+	}
+
+	o.Price, err = strconv.ParseFloat(raw[0], 64)
+	if err != nil {
+		log.Println("ERROR: Converting price to float", err)
+	}
+	o.Quantity, err = strconv.ParseFloat(raw[1], 64)
+	if err != nil {
+		log.Println("ERROR: Converting quantity to int", err)
+	}
+
+	return nil
+}
+
+type Answer struct {
+	EventType string		`json:"e"`
+	EventTime int64			`json:"E"`
+	TransactionTime int64	`json:"T"`
+	Symbol string			`json:"s"`
+	FirstUpdateID int64		`json:"U"`
+	LastUpdateID int64		`json:"u"`
+	PrevFinalID int64		`json:"pu"`
+	Bids []Order			`json:"b"`
+	Asks []Order			`json:"a"`
+}
 
 type WsClient struct {
 	websocket *websocket.Conn
@@ -15,30 +52,29 @@ type WsClient struct {
 	done chan struct{}
 }
 
-var wsclient WsClient
-
-func receive() {
-	defer close(wsclient.done)
+func (c *WsClient) receive() {
+	defer close(c.done)
 	for {
-		_, msg, err := wsclient.websocket.ReadMessage()
+		_, msg, err := c.websocket.ReadMessage()
 		if err != nil {
-			log.Println("[RCV]: Error recieving message", err)
+			log.Println("ERROR: [RCV]: Error recieving message", err)
+			close(c.messages)
 			return
 		}
 
-		wsclient.messages <- msg
+		c.messages <- msg
 	}
 }
 
-func parseAndPass() {
-	defer close(wsclient.done)
+func (c *WsClient) parseAndPass() {
+	defer close(c.done)
 	for {
-		msg := <- wsclient.messages
+		msg := <- c.messages
 
 		var update Answer
 		err := json.Unmarshal(msg, &update)
 		if err != nil {
-			log.Fatal("[JSON] Error parsing json", err)
+			log.Println("ERROR: [JSON] Error parsing json", err)
 		}
 
 		log.Println(update.Symbol, update.Bids[0])
@@ -48,19 +84,21 @@ func parseAndPass() {
 }
 
 func main() {
+	var wsclient WsClient
+
 	ws, res, err := websocket.DefaultDialer.Dial("wss://fstream.binance.com/ws/btcusdt@depth@100ms", nil)
 	if err != nil {
 		log.Fatal("[DIAL] Couldn't connect to Binance API:", err)
 	}
 	wsclient.websocket = ws
-	wsclient.messages = make(chan []byte)
+	wsclient.messages = make(chan []byte, 100)
 	defer wsclient.websocket.Close()
 
 	wsclient.done = make(chan struct{})
 
-	go receive()
+	go wsclient.receive()
 
-	go parseAndPass()
+	go wsclient.parseAndPass()
 
 	<-wsclient.done
 
