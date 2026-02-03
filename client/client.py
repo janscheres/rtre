fc51d8c99df9a29b37042db7cff990dffa2c1dce
Jan Scheres <jscheresm@gmail.com>
2026-02-03 22:12:07 +0000

    Basic standard deviation calculation

--- client/client.py
+++ client/client.py
@@ -10,11 +10,29 @@ def main():
 
     request = rtre_pb2.RiskRequest(symbol="btcusdt")
 
+    obiArr = []
+    obiSum = 0
+    numObi = 0
+    mean = 0
+
     try:
         for response in stub.StreamRisk(request):
-            print("Received:", response.timestamp, response.obi)
+            obiArr.append(response.obi)
+            obiSum+=response.obi
+            numObi+=1
+
+            mean = obiSum/numObi
+
+            print("Received:", response.timestamp, response.obi, mean)
+
     except grpc.RpcError as e:
         print("gRPC error:", e)
+
+    sumOfSquares=0
+    for o in obiArr:
+        sumOfSquares+= pow((o-mean), 2)
+
+    print("sample standard deviation:", pow(sumOfSquares/(numObi-1), 0.5))
 
 
 if __name__ == "__main__":
